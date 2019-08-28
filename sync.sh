ed931a1f7c3bb457a2bf5dc8faf776190d4d07da
fortinetps <dyao@fortinet.com>
2019-08-28 16:16:11 +0000

    fortios_facts unit test module

--- sync.sh
+++ sync.sh
@@ -1,6 +1,19 @@
 export ansible_lib="/usr/local/lib/python3.7/site-packages/ansible"
+export ansible_dev_lib="/workspaces/ansible/lib/ansible"
+export ansible_dev_test="/workspaces/ansible/test"
+
 cp -r /workspaces/fortios_monitor/lib/ansible/* $ansible_lib/
+cp -r /workspaces/fortios_monitor/lib/ansible/* $ansible_dev_lib/
+cp -r /workspaces/fortios_monitor/test/* $ansible_dev_test/
+
 ls -l $ansible_lib/modules/network/fortios/fortios_facts.py
 ls -lR $ansible_lib/module_utils/network/fortios/argspec/*
 ls -lR $ansible_lib/module_utils/network/fortios/facts/*
+
+ls -l $ansible_dev_lib/modules/network/fortios/fortios_facts.py
+ls -lR $ansible_dev_lib/module_utils/network/fortios/argspec/*
+ls -lR $ansible_dev_lib/module_utils/network/fortios/facts/*
+
+ls -lR $ansible_dev_test/units/modules/network/fortios/test_fortios_facts.py
+
 # code $ansible_lib/modules/network/fortios/fortios_facts.py
--- test/units/modules/network/fortios/test_fortios_facts.py
+++ test/units/modules/network/fortios/test_fortios_facts.py
@@ -21,6 +21,10 @@ import os
 import json
 import pytest
 from mock import ANY
+from units.modules.utils import exit_json, fail_json
+from units.compat import unittest
+from units.compat.mock import patch
+from ansible.module_utils import basic
 from ansible.module_utils.network.fortios.fortios import FortiOSHandler
 
 try:
@@ -42,32 +46,58 @@ fos_instance = FortiOSHandler(connection_mock)
 def test_facts_get(mocker):
     monitor_method_result = {'status': 'success', 'http_method': 'GET', 'http_status': 200}
     monitor_method_mock = mocker.patch('ansible.module_utils.network.fortios.fortios.FortiOSHandler.monitor', return_value=monitor_method_result)
+    mock_module = patch.multiple(basic.AnsibleModule, exit_json=exit_json, fail_json=fail_json)
+    mock_module._connection = connection_mock
 
-    input_data = {
-        "username": "admin",
-        "vdom": "root",
-        "gather_subset": [
-            {"fact": "system_status_select"},
+    # test case 01, args with single gather_subset
+    args = {
+        'vdom': 'root',
+        'gather_subset': [
+            {'fact': 'system_status_select'},
+        ]
+    }
+    mock_module.params = args
+
+    response, ignore = Facts(mock_module, fos_instance).get_facts()
+
+    monitor_method_mock.assert_called_with('system', 'status/select', vdom='root')
+    assert response['ansible_network_resources']['system_status_select']['status'] == 'success'
+    assert response['ansible_network_resources']['system_status_select']['http_status'] == 200
+
+    # test case 02, args with single gather_subset with filters
+    args = {
+        'vdom': 'root',
+        'gather_subset': [
+            {'fact': 'system_interface_select', 'filters': [{'include_vlan': 'true'}, {'interface_name': 'port3'}]},
         ]
     }
 
-    response = Facts(input_data, fos_instance).get_facts()
+    mock_module.params = args
 
-    monitor_method_mock.assert_called_with('system', 'status/select', vdom='root')
-    assert response['status'] == 'success'
-    assert response['http_status'] == 200
+    response, ignore = Facts(mock_module, fos_instance).get_facts()
 
-    # input_data = {
-    #     "username": "admin",
-    #     "vdom": "root",
-    #     "gather_subset": [
-    #         {"fact": "system_current-admins_select"},
-    #         {"fact": "system_firmware_select"},
-    #         {"fact": "system_fortimanager_status"},
-    #         {"fact": "system_ha-checksums_select"},
-    #         {"fact": "system_interface_select", "filters": [{"include_vlan": "true"}, {"interface_name": "port3"}]},
-    #         {"fact": "system_status_select"},
-    #         {"fact": "system_time_select"},
-    #     ]
-    # }
+    monitor_method_mock.assert_called_with('system', 'interface/select?vdom=root&include_vlan=true&interface_name=port3', vdom=None)
+    assert response['ansible_network_resources']['system_interface_select']['status'] == 'success'
+    assert response['ansible_network_resources']['system_interface_select']['http_status'] == 200
 
+    # test case 03, args with multiple gather_subset
+    args = {
+        'vdom': 'root',
+        'gather_subset': [
+            {'fact': 'system_current-admins_select'},
+            {'fact': 'system_firmware_select'},
+            {'fact': 'system_fortimanager_status'},
+            {'fact': 'system_ha-checksums_select'},
+        ]
+    }
+
+    mock_module.params = args
+
+    response, ignore = Facts(mock_module, fos_instance).get_facts()
+
+    monitor_method_mock.assert_any_call('system', 'current-admins/select', vdom='root')
+    monitor_method_mock.assert_any_call('system', 'firmware/select', vdom='root')
+    monitor_method_mock.assert_any_call('system', 'fortimanager/status', vdom='root')
+    monitor_method_mock.assert_any_call('system', 'ha-checksums/select', vdom='root')
+    assert response['ansible_network_resources']['system_ha-checksums_select']['status'] == 'success'
+    assert response['ansible_network_resources']['system_ha-checksums_select']['http_status'] == 200
